8c88e1e535d97f5c94f7ac7de39f6851e4696860
Hung Tang <hungtang@hotmail.com>
2021-06-01 17:20:04 -0700

    building some basic files

---
+++ models/User.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ] 
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        }
+
+}
+
+
+})
